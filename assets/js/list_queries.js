438ef14d89a40cc457c5933bd6514b2d1ab6e82f
MeenuSowjanya <buvaneswarchandrasekaran@gmail.com>
2021-12-20 19:08:10 +0530

    Fixed sonarcloud issues

--- assets/js/list_queries.js
+++ assets/js/list_queries.js
@@ -12,10 +12,11 @@ function get(x) {
     array.includes("w") &&
     array.includes("n")
   ) {
-    document.getElementById(x).style.border = "tomato";
-    document.getElementById(x).style.backgroundColor = "tomato";
+    document.getElementById(x).style.border = "orange";
+    document.getElementById(x).style.backgroundColor = "orange";
   }
 }
+
 for (let i of localStorageGet) {
   let date = i.createdAt;
   let name = i.title;
@@ -36,26 +37,4 @@ for (let i of localStorageGet) {
 console.log(str);
 let div = document.getElementById("new");
 div.innerHTML = str;
-// axios
-//   .get("https://61bb259de943920017784cc7.mockapi.io/query")
-//   .then(function (res) {
-//     console.table(res.data);
-//     object = res.data.length;
-//     for (let i of res.data) {
-//       console.table(i);
-//       let date = i.createdAt;
-//       let name = i.title;
-//       let desc = i.desc;
-//       let tag = i.tag;
-//       str +=
-//         "<div class='kitkat'><p class='Date'>" +
-//         date +
-//         "</p><p class='User'>" +
-//         name +
-//         "</p><span> <p class='tag'>" +
-//         tag +
-//         "</p> </span><span><p class='desc'>" +
-//         desc +
-//         "</p></span></div>";
-//     }
-//   });
+

954cac9cc5f5c36d666f2bfd250ba78e536d16ef
MeenuSowjanya <buvaneswarchandrasekaran@gmail.com>
2022-01-14 15:47:12 +0530

    Changed ui for my pages

--- assets/js/create_not.js
+++ assets/js/create_not.js
@@ -1,41 +1,73 @@
- function reload() {
-        window.location.reload();
-      }
-      function submitHandler(event) {
-        event.preventDefault();
-        if (localStorage.getItem("notifications") == null) {
-          localStorage.setItem("notifications", JSON.stringify([]));
-          let title = document.getElementById("title").value;
-          let data = document.getElementById("data").value;
-          let st = document.getElementById("st").value;
-          let date = new Date();
-          updatePage(title, data, date, st);
-          alert("Notification has been sent to everybody !");
-          window.location.href = "../pages/notification.html";
-        } else {
-          let title = document.getElementById("title").value;
-          let data = document.getElementById("data").value;
-          let date = new Date();
-          let st = document.getElementById("st").value;
-          updatePage(title, data, date, st);
-          alert("Notification has been sent to everybody !");
-          window.location.href = "../pages/notification.html";
-        }
-      }
+function reload() {
+  window.location.reload();
+}
+function submitHandler(event) {
+  event.preventDefault();
+  document.getElementById("submitBtn").style.backgroundColor = "#46a9a9";
+  document.getElementById("submitBtn").style.color = "white";
+  if (localStorage.getItem("notifications") == null) {
+    localStorage.setItem("notifications", JSON.stringify([]));
+    let title = document.getElementById("title").value;
+    let data = document.getElementById("data").value;
+    let st = document.getElementById("st").value;
+    let date = new Date();
+    updatePage(
+      title,
+      data,
+      date.getDate(),
+      date.getMonth() + 1,
+      date.getFullYear(),
+      date.getHours(),
+      date.getMinutes(),
+      st
+    );
+    alert("Notification has been sent to everybody !");
+    window.location.href = "../pages/notification.html";
+  } else {
+    let title = document.getElementById("title").value;
+    let data = document.getElementById("data").value;
+    let date = new Date();
+    let st = document.getElementById("st").value;
+    updatePage(
+      title,
+      data,
+      date.getDate(),
+      date.getMonth() + 1,
+      date.getFullYear(),
+      date.getHours(),
+      date.getMinutes(),
+      st
+    );
+    alert("Notification has been sent to everybody !");
+    window.location.href = "../pages/notification.html";
+  }
+}
 
-      function updatePage(add_title, add_data, add_date, add_sub) {
-        let str = getAllFields();
-        let object = {
-          title: add_title,
-          data: add_data,
-          date: add_date,
-          sub: add_sub,
-        };
-        str.push(object);
-        console.log(str);
-        localStorage.setItem("notifications", JSON.stringify(str));
-      }
-      function getAllFields() {
-       return (JSON.parse(localStorage.getItem("notifications")));
-      }
-      
+function updatePage(
+  add_title,
+  add_data,
+  add_date,
+  add_month,
+  add_years,
+  add_hours,
+  add_minutes,
+  add_sub
+) {
+  let str = getAllFields();
+  let object = {
+    title: add_title,
+    data: add_data,
+    date: add_date,
+    month: add_month,
+    year: add_years,
+    hour: add_hours,
+    minutes: add_minutes,
+    sub: add_sub,
+  };
+  str.push(object);
+  console.log(str);
+  localStorage.setItem("notifications", JSON.stringify(str));
+}
+function getAllFields() {
+  return JSON.parse(localStorage.getItem("notifications"));
+}
--- assets/js/list_notification.js
+++ assets/js/list_notification.js
@@ -1,19 +1,21 @@
-let lS=JSON.parse(localStorage.getItem("notifications"));
-     let str="";
-     for(let i of lS){
-      
-      str+=`<div class='grid-container-element12'>
-              <div class='a1'>
-                  <img src='../assets/images/bell.png' height='50px' width='50px'></div>
-                  <div class='a2'>${i.title}</div>
-                  <br><br>
-                  <div class='a3'>
-                    <span class='w12'>${i.sub}</span>
-                    <span class='w13'><a href='#'>${i.data}</a></span>
-                    <span class='w14'>${i.date}</span>
-                    </div>
-                    </div>
-                    <br>`;
-     }
-     console.log(str);
-     document.getElementById("new").innerHTML=str;
+function redirect() {
+  document.getElementById("redirect").style.backgroundColor = "#ec745a";
+  window.location.href = "./../pages/create_not.html";
+}
+let lS = JSON.parse(localStorage.getItem("notifications"));
+let str = "";
+for (let i of lS) {
+  str += `<div class="grid-container-element12">
+  <div class="a1"><img src="../assets/images/bell.png" height="50px" width="50px" alt=""></div>
+  <div class="a2">
+      ${i.title}
+  </div><br><br>
+  <div class="a3">
+     <span class="w12">${i.sub} </span><span class="w13"> <a href="https://freshworks.zoom.us/my/b2classroom">${i.data}</a></span>
+     <span class="w14">${i.hour}:${i.minutes}(${i.date}/${i.month}/${i.year}) </span>
+  </div>
+
+</div>`;
+}
+console.log(str);
+document.getElementById("new").innerHTML = str;

10504ddf304c357383abb7d358b358e99eaeb2b4
MeenuSowjanya <buvaneswarchandrasekaran@gmail.com>
2022-01-17 13:45:17 +0530

    Added escape sequence for my innerhtml code snippets to improve its quality

--- assets/js/list_notification.js
+++ assets/js/list_notification.js
@@ -5,13 +5,13 @@ function redirect() {
 let lS = JSON.parse(localStorage.getItem("notifications"));
 let str = "";
 for (let i of lS) {
-  str += `<div class="grid-container-element12">
-  <div class="a1"><img src="../assets/images/bell.png" height="50px" width="50px" alt=""></div>
-  <div class="a2">
+  str += `<div class=\"grid-container-element12\">
+  <div class=\"a1\"><img src=\"../assets/images/bell.png\" height=\"50px\" width=\"50px\" alt=\"\"></div>
+  <div class=\"a2\">
       ${i.title}
   </div><br><br>
-  <div class="a3">
-     <span class="w12">${i.sub} </span><span class="w13"> <a href="https://freshworks.zoom.us/my/b2classroom">${i.data}</a></span>
+  <div class=\"a3\">
+     <span class=\"w12\">${i.sub} </span><span class=\"w13\"> <a href=\"https://freshworks.zoom.us/my/b2classroom\">${i.data}</a></span>
      <span class="w14">${i.hour}:${i.minutes}(${i.date}/${i.month}/${i.year}) </span>
   </div>
 
